e55eee22b777f6ef4f64a7589aad2f62c584fb68
Jeff Hui <jeff@jeffhui.net>
2016-10-20 22:03:22 -0700

    Remove classifier

--- setup.py
+++ setup.py
@@ -39,6 +39,5 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Testing',
-        'Topic :: Software Development :: Libraries',
     ],
 )
